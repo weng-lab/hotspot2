b73c26ff6e4405373589cb36988bdaa4495bcf3f
sjneph <sjneph@gmail.com>
2016-03-09 09:07:05 -0800

    clean up files; get rid of gzip-starch files (new bug)

--- scripts/cutcounts.bash
+++ scripts/cutcounts.bash
@@ -3,7 +3,7 @@
 set -x -e -o pipefail
 
 if [[ $# -lt 2 ]] ; then
-  echo "Usage: $0 in.bam mappabilityFile.bed" >&2
+  echo "Usage: $0 in.bam output.starch" >&2
   exit 2
 fi
 
@@ -16,12 +16,12 @@ awk_cut="$(dirname $0)/cutfragments.awk"
 name=$(basename $bam .bam)
 outputdir="$(dirname $bam)"
 
-CUTS_BED=$outputdir/$name.cuts.sorted.bed.starch
 FRAGMENTS=$outputdir/$name.fragments.sorted.bed.starch
 
 if [[ -z "$TMPDIR" ]] ;then
   TMPDIR=$(mktemp -d)
 fi
+CUTS_BED=$TMPDIR/$name.cuts.sorted.bed
 
 # temp files
 CUTSTMP="$TMPDIR/cuts.bed"
@@ -32,8 +32,11 @@ if [[  ! -s "$CUTS_BED" || ! -s "$FRAGMENTS" ]]; then
 
   time bam2bed --do-not-sort < "$bam" \
     | awk -v "cutfile=$CUTSTMP" -v "fragmentfile=$FRAGMENTSTMP" -f "$awk_cut"
-  sort-bed --max-mem 16G "$FRAGMENTSTMP" | starch --gzip - > "$FRAGMENTS"
-  sort-bed --max-mem 16G "$CUTSTMP"      | starch --gzip - > "$CUTS_BED"
+  sort-bed --max-mem 8G "$FRAGMENTSTMP" | starch - > "$FRAGMENTS"
+  sort-bed --max-mem 8G "$CUTSTMP"      > "$CUTS_BED"
+
+  rm -f "$CUTSTMP"
+  rm -f "$FRAGMENTSTMP"
 
 fi
 
@@ -42,7 +45,8 @@ if [[ ! -s "$CUTCOUNTS" ]]; then
   time bedops --chop 1 "$CUTS_BED" \
     | bedmap --faster --echo --count --delim "\t" - "$CUTS_BED" \
     | awk '{print $1"\t"$2"\t"$3"\tid-"NR"\t"$4}' \
-    | starch --gzip - \
+    | starch - \
     > "$CUTCOUNTS"
 
+  rm -f "$CUTS_BED"
 fi
