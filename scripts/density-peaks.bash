992f922a9ba9c8cb3b7ee1b3e4bdd1bc1915e924
sjneph <sjneph@gmail.com>
2016-03-12 15:48:53 -0800

    density-peaks --> loop through everything by chrom and collate results at the end of script

--- scripts/density-peaks.bash
+++ scripts/density-peaks.bash
@@ -2,7 +2,7 @@
 set -x -e -o pipefail
 
 if [[ $# != 6 ]] ; then
-  echo "Usage: $0 tmpdir wavelets-binary tags.starch chrom-sizes.starch density-out.starch peaks-out.starch" >&2
+  echo "Usage: $0 tmpdir wavelets-binary <tags.starch> <chrom-sizes.starch> <density-out.starch> <peaks-out.starch>" >&2
   exit 2
 fi
 
@@ -28,32 +28,31 @@ wavletlvl=3
 filter_type=Haar
 boundary_type=reflected
 
-
-## Tag density, 150bp window, sliding every 20bp, used for peak-finding.
-echo "calculating densities..."
-bedops --ec -u $chrfile \
-  | awk -v b=$bins -v s=$step \
-     'BEGIN {OFS="\t"; hs=s/2; hb=b/2} ; { \
-       for ( start = $2+hb-hs; start < $3-hb-hs; start+=s) { \
-         print $1, start, start+s, "."; \
-       } \
-     }' \
-  | bedmap --faster --range $rangepad --delim "\t" --echo --count - $tags \
-  | starch - \
- > $density
-
-echo "peak-finding..."
-outs=""
+echo "density and peak-finding..."
+pkouts=""
+densouts=""
 for chr in $(bedops --ec -u $chrfile | cut -f1)
 do
-  rm -f $tmpdir/.waves
+  ## Tag density, 150bp window, sliding every 20bp, used for peak-finding.
+  bedops --ec -u --chrom $chr $chrfile \
+    | awk -v b=$bins -v s=$step \
+       'BEGIN {OFS="\t"; hs=s/2; hb=b/2} ; { \
+         for ( start = $2+hb-hs; start < $3-hb-hs; start+=s) { \
+           print $1, start, start+s, "."; \
+         } \
+       }' \
+    | bedmap --faster --chrom $chr --range $rangepad --delim "\t" --echo --count - $tags \
+    | starch - \
+   > $tmpdir/.dens.$chr.starch
 
-  unstarch $chr $density \
+  densouts="$densouts $tmpdir/.dens.$chr.starch"
+
+  unstarch $tmpdir/.dens.$chr.starch \
     | cut -f5 \
     | wavelets --level $waveletlvl --to-stdout --boundary $boundary_type --filter $filter_type - \
     > $tmpdir/.waves
 
-  unstarch $chr $density \
+  unstarch $tmpdir/.dens.$chr.starch \
     | paste - $tmpdir/.waves \
     | cut -f1-4,6 \
     | awk 'BEGIN{incr=0} ; { \
@@ -70,13 +69,18 @@ do
           }' \
     > $tmpdir/.wave.$chr
 
-  outs="$outs $tmpdir/.wave.$chr"
+  pkouts="$pkouts $tmpdir/.wave.$chr"
+
+  rm -f $tmpdir/.waves
 done
 
-cat $outs \
+cat $pkouts \
   | awk -v h=$halfbin '{m=($2+$3)/2; left=m-h; if(left < 0) left=0; print $1"\t"left"\t"m+h"\t"$4"\t"$5}' - \
   | starch - \
   > $pk
+
+starchcat $densouts \
+  > $density
 
 #need to add pph
 
