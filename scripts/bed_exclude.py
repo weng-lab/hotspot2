5ac1a22fb79028a97a2479521c6f94a7068acf60
Jemma Nelson <nelsonjs@uw.edu>
2016-03-19 20:00:50 -0700

    Fixups for bed_exclude.py

--- scripts/bed_exclude.py
+++ scripts/bed_exclude.py
@@ -3,7 +3,7 @@
 import sys
 line = sys.stdin.readline()
 ba = line.split()
-with open("exclude_regions.bed") as x:
+with open(sys.argv[1]) as x:
     x_line = x.readline()
     xa = x_line.split()
 
@@ -14,17 +14,28 @@ with open("exclude_regions.bed") as x:
             ba = line.split()
             continue
 
-        # fast-forward excludes
-        if (xa[0] < ba[0] or int(xa[2]) <= int(ba[1])):
-            x_line = x.readline()
-            xa = x_line.split()
+        # Check for different chromosomes
+        if (xa[0] != ba[0]):
+            if (xa[0] < ba[0]):
+                x_line = x.readline()
+                xa = x_line.split()
+            else:
+                print("\t".join(ba))
+                line = sys.stdin.readline()
+                ba = line.split()
             continue
 
         # print and fast-forward bed
-        if (ba[0] < xa[0] or int(ba[2]) <= int(xa[1])):
+        if (int(ba[2]) <= int(xa[1])):
             print("\t".join(ba))
             line = sys.stdin.readline()
             ba = line.split()
+            continue
+
+        # fast-forward excludes
+        if (int(xa[2]) <= int(ba[1])):
+            x_line = x.readline()
+            xa = x_line.split()
             continue
 
         # If exclude enitrely covers bed, skip to next bed
@@ -35,7 +46,7 @@ with open("exclude_regions.bed") as x:
 
         # If bed entirely covers exclude, print out first part and modify
         if int(ba[2]) >= int(xa[1]) and int(ba[1]) <= int(xa[1]):
-            print "\t".join([ba[0], ba[1], xa[1], ba[3], ba[4]])
+            print("\t".join([ba[0], ba[1], xa[1], ba[3], ba[4]]))
             ba[1] = xa[2]
             continue
 
@@ -47,12 +58,12 @@ with open("exclude_regions.bed") as x:
         # If exclude covers last part of bed, print modified and read next
         if (int(xa[1]) <= int(ba[2])):
             ba[2] = xa[1]
-            print "\t".join(ba)
+            print("\t".join(ba))
             continue
 
         raise Exception("Coding error: Should never get here")
 
 # Print out remaining lines
 while line:
-    print(line)
+    sys.stdout.write(line)
     line = sys.stdin.readline()
